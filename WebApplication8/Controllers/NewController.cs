da5ea70dc708322bdfdfdfb3319486adfc0b98bb
psy <psy>
2019-11-11 22:13:38 +0100

    first commit

---
+++ WebApplication8/Controllers/NewController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication8.Infrastructure;
+
+namespace WebApplication8.Controllers
+{
+    public class NewController: Controller
+    {
+        public NewController()
+        {
+
+        }
+
+        public IActionResult About() 
+        {
+
+            return this.RedirectTo<DefaultController>(x => x.Index());
+
+           // return RedirectToAction("Index", "Default");
+        }
+    }
+}
---
+++ WebApplication8/Infrastructure/ControllerExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq.Expressions;
+
+namespace WebApplication8.Infrastructure
+{
+    public static class ControllerExtensions
+    {
+        public static IActionResult RedirectTo<TController>(this Controller controller, Expression<Action<TController>> redurectExpression)
+        {
+            if (redurectExpression.Body.NodeType != ExpressionType.Call) 
+            {
+                throw new InvalidOperationException($"the provided expression is not a valid method call: {redurectExpression.Body}");
+            }
+
+            var methodCallExpression = (MethodCallExpression)redurectExpression.Body;
+
+            var actionName = methodCallExpression.Method.Name;
+            var controllerName = typeof(TController).Name.Replace(nameof(Controller),string.Empty);
+
+            return controller.RedirectToAction(actionName, controllerName);
+        }
+    }
+}
